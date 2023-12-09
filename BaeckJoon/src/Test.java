20c70065453b48ce512c158f39ecff6bfe67b93b
TH_97 <hood3392@gmail.com>
2023-12-09 16:07:57 +0900

    Feat: 깃 이그노어 생성

---
+++ BaeckJoon/src/Test.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class Test {
+    public static void main(String[] args) {
+       int i = 10;
+       int j = -11;
+       System.out.println(i+j);
+    }
+}
